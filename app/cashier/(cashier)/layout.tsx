29a1f456220961a09664e1b1a988a8b239ae5d20
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2026-05-13 03:19:55 -0700

    updates

--- app/cashier/(cashier)/layout.tsx
+++ app/cashier/(cashier)/layout.tsx
@@ -19,25 +19,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getUserSession();
-  const role = session.user.role;
-
-  if (role !== "cashier") {
-    if (role === "store") {
-      redirect(`/store`);
-    } else if (role === "customer") {
-      redirect(`/`);
-    } else if (role === "admin") {
-      redirect(`/admin`);
-    } else {
-      redirect("/cashier/login");
-    }
-  }
   return (
     <div className="scroll-smooth">
       <TooltipProvider>
         <div className="min-h-screen bg-gray-50">
-          <main className="md:ml-64 pt-14 md:pt-0 min-h-screen m-4">
+          <main className="md:ml-64 pt-14 md:pt-0 min-h-screen">
             {children}
           </main>
         </div>
