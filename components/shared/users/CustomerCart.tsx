8dde54eed8536fd5103ad0163f45a18fa09d4d5f
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2026-06-14 08:43:06 -0700

    fixes on wallet topup

--- components/shared/users/CustomerCart.tsx
+++ components/shared/users/CustomerCart.tsx
@@ -224,15 +224,15 @@ const CustomerCart = async ({ customerId }: { customerId?: string }) => {
   // console.log(miscTotals)
 
   const totals = {
-    subtotal:
-      itemTotals.subtotal + subsidyTotals.subtotal + miscTotals.subtotal,
+    subtotal: Math.round(itemTotals.subtotal + subsidyTotals.subtotal + miscTotals.subtotal),
     gst: itemTotals.gst + subsidyTotals.gst + miscTotals.gst,
     pst: itemTotals.pst + subsidyTotals.pst + miscTotals.pst,
     totalTax:
       itemTotals.totalTax + subsidyTotals.totalTax + miscTotals.totalTax,
     disposable: itemTotals.disposable + subsidyTotals.disposable,
-    total: itemTotals.total + subsidyTotals.total + miscTotals.total,
+    total: Math.round(itemTotals.total + subsidyTotals.total + miscTotals.total),
   };
+
 
   const showGST = totals.gst > 0;
   const showPST = totals.pst > 0;
